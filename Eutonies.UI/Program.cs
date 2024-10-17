fe62695385c52343344e4fecc84fa98f2d33cdf7
Sune Roenne <sune_roenne@outlook.com>
2024-10-17 19:57:50 +0200

    .k8s and others

--- Eutonies.UI/Program.cs
+++ Eutonies.UI/Program.cs
@@ -10,20 +10,15 @@ builder.Services.AddSingleton<IWordsLoader, WordsLoader>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapBlazorHub()
+   .WithOrder(-1);
+
+app.UseRouting();
+app.UseAntiforgery();
 
 app.Run();
